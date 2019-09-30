d68474a4c1c892ade56d63fce6e0004fa7d46761
6442742@qq.com <qwertyuiop>
2019-09-30 15:46:15 +0800

    第一题

---
+++ practice/1. 两数之和.py
@@ -0,0 +1,31 @@
+"""
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+示例:
+
+给定 nums = [2, 7, 11, 15], target = 9
+
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+        for i, j in enumerate(nums):
+            if target-j in d:
+                return [d[target-j], i]
+            d[j] = i
+
+    # 简单解法
+    # for i in range(len(nums)):
+    #     for j in range(i + 1, len(nums)):
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
